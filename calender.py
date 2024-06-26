d20fd8f7875ded2f9581c8e2657c87ff7ac9b822
Monu Prajapati <pmonu4041@gmail.com>
2024-06-26 12:07:56 +0530

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,7 @@
+import calendar
+x=int(input("Enter the year := "))
+y=int(input("Enter the month := "))
+# year = 2024
+# print (calendar.calendar(x))
+print(calendar.month(x,y))
+print (calendar.calendar(x))
